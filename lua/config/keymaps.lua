5ad0c9f037aaf552ee666b329f6cac519e688511
soliprem <franci.solidoro@gmail.com>
2023-05-09 16:39:02 +0200

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,6 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 map("n", "<leader>ai", ':AsyncRun nvim-gpt ""<left>')
-map("n", "<leader>v", "<cmd>r! dm-names<cr>i<right> ")
+map("n", "<leader>v", "<cmd>r! names<cr>i<right> ")
 map("n", "<leader>ute", "<cmd>set spelllang=en<cr>")
 map("n", "<leader>uti", "<cmd>set spelllang=it.utf-8.spl<cr>")
