2dfc4ca2a3e7a68a57392f30769caac93ad5de8f
soliprem <franci.solidoro@gmail.com>
2023-06-01 17:21:44 +0200

    update

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,18 @@
 return {
   {
     "stevearc/oil.nvim",
+    event = verylazy,
     opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
-    end,
+    keys = {
+      {
+        "-",
+        function()
+          require("oil").open()
+        end,
+        { desc = "Open parent directory" },
+      },
+    },
   },
 }
