51bd9ebb85c99b4d2693f0d0b34549664a2f9a8d
soliprem <franci.solidoro@gmail.com>
2023-12-05 04:00:30 +0100

    added neorg

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,7 @@ return { -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   { "deviantfero/wpgtk.vim" },
   { "AlphaTechnolog/pywal.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
   { "catppuccin/nvim", name = "catppuccin" },
   -- Configure LazyVim to load gruvbox
   {
