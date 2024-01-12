e2ed5092b425cc28e62d0aaa57b36f9303026d1a
Soliprem <franci.solidoro@gmail.com>
2024-01-12 10:28:11 +0100

    new stuff

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -1,6 +1,6 @@
 return {
-	"jbyuki/nabla.nvim",
-	module = "nabla",
-	-- stylua: ignore
-	-- keys = { "<leader>pp", function() require("nabla").popup() end, desc = "nabla popup" },
+  "jbyuki/nabla.nvim",
+  module = "nabla",
+  -- stylua: ignore
+  -- keys = { "<leader>pp", function() require("nabla").popup() end, desc = "nabla popup" },
 }
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -6,9 +6,10 @@ return {
     config = function()
       require("neorg").setup({
         load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.defaults"] = {},  -- Loads default behaviour
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
+          ["core.export"] = {},    -- Adds export options
+          ["core.dirman"] = {      -- Manages Neorg workspaces
             config = {
               workspaces = {
                 notes = "~/Documents/neorg",
