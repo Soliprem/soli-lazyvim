c5a9ca616c77144b2c325cab6f1d2b6dfff70270
soliprem <franci.solidoro@gmail.com>
2024-03-10 16:14:42 +0100

    update

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,8 +1,15 @@
 return {
   {
+    "vhyrro/luarocks.nvim",
+    branch = "more-fixes",
+    config = function()
+      require("luarocks").setup({})
+    end,
+  },
+  {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
-    dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
+    dependencies = { { "nvim-neorg/neorg-telescope" }, { "luarocks.nvim" }, { "3rd/image.nvim" } },
     config = function()
       require("neorg").setup({
         load = {
@@ -10,6 +17,8 @@ return {
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
           ["core.export"] = {}, -- Adds export options
           ["core.integrations.telescope"] = {},
+          ["core.integrations.image"] = {},
+          -- ["core.latex.renderer"] = {},
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
               workspaces = {
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,29 +1,29 @@
 return {
-  "epwalsh/obsidian.nvim",
-  requires = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
-  },
-  config = function()
-    require("obsidian").setup({
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/vaults/personal",
-        },
-        {
-          name = "work",
-          path = "~/vaults/work",
-        },
-        {
-          name = "vimwiki",
-          path = "~/vimwiki",
-        }
-      },
-
-      -- see below for full list of options 👇
-    })
-  end,
+  -- "epwalsh/obsidian.nvim",
+  -- requires = {
+  --   -- Required.
+  --   "nvim-lua/plenary.nvim",
+  --
+  --   -- see below for full list of optional dependencies 👇
+  -- },
+  -- config = function()
+  --   require("obsidian").setup({
+  --     workspaces = {
+  --       {
+  --         name = "personal",
+  --         path = "~/vaults/personal",
+  --       },
+  --       {
+  --         name = "work",
+  --         path = "~/vaults/work",
+  --       },
+  --       {
+  --         name = "vimwiki",
+  --         path = "~/vimwiki",
+  --       }
+  --     },
+  --
+  --     -- see below for full list of options 👇
+  --   })
+  -- end,
 }
